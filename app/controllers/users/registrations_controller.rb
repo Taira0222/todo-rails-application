412c50ec611362cefaff6e21f9631ff89e3ee160
Taira Aikawa <tir.3t.f22@gmail.com>
2025-05-18 20:33:12 +0000

    Finish login-function without login integration test

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
+  # before_action :configure_account_update_params, only: [:update]
 
   def new
     super
@@ -13,20 +13,20 @@ class Users::RegistrationsController < Devise::RegistrationsController
     super
   end
 
-  # GET /resource/edit
-  def edit
-    super
-  end
+  # # GET /resource/edit
+  # def edit
+  #   super
+  # end
 
-  # PUT /resource
-  def update
-    super
-  end
+  # # PUT /resource
+  # def update
+  #   super
+  # end
 
-  # DELETE /resource
-  def destroy
-    super
-  end
+  # # DELETE /resource
+  # def destroy
+  #   super
+  # end
 
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
@@ -45,14 +45,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  end
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  # end
 
   # The path used after sign up.
-  def after_sign_up_path_for(resource)
-    super(resource)
-  end
+  # def after_sign_up_path_for(resource)
+  #   super(resource)
+  # end
 
   # サインアップをした後の画面遷移先
   def after_inactive_sign_up_path_for(resource)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  # flashメッセージの色分けロジック
+  def flash_color_class(type)
+    case type.to_sym
+    when :notice
+      'bg-blue-100 text-blue-700 border-blue-300'
+    when :success
+      'bg-green-100 text-green-700 border-green-300'
+    when :alert, :error
+      'bg-red-100 text-red-700 border-red-300'
+    else
+      'bg-gray-100 text-gray-700 border-gray-300'
+    end
+  end
 end
