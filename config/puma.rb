d9395a64ee23ad8928411296275fb4b00e2c9a15
Taira Aikawa <tir.3t.f22@gmail.com>
2025-05-23 00:39:46 +0000

    Modified puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,10 @@
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
-threads threads_count, threads_count
-port ENV.fetch("PORT", 3000)
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+threads min_threads_count, max_threads_count
+port
+ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RAILS_ENV") { "development" }
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+workers ENV.fetch("WEB_CONCURRENCY") { 4 }
+preload_app!
 plugin :tmp_restart
-plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
-pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
-plugin :tailwindcss if ENV.fetch("RAILS_ENV", "development") == "development"
